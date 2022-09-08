74b9e2870a406031f54facfd6c22897c85d02f22
SKuger57 <samuel.kuger@upb.edu.co>
2022-09-08 14:51:44 -0500

    Solucion error

--- problem/program.asm
+++ problem/program.asm
@@ -2,12 +2,12 @@
 // 000416026
 // samuel.kuger@upb.edu.co
 
-(INICIO) // Inicia la variable de las 2 teclas y en caso de que una sea oprimida dice que debe ejecutar "VACIO" o "LINEAS", es un loop, o sea, hasta que no se ordene algo no pasa de aca.
+(INICIO) 
 		@KBD
 		D=M
 		@84
 		D=D-A
-		@LINEAS
+		@VERT
 		D;JEQ
 
 		@KBD
@@ -20,7 +20,7 @@
 		@INICIO
 		0;JMP
 
-(VACIO) // Indica las cordenadas y el punto desde donde se comienza a borrar haciendo un salto a la funcion de borrarse y reinicia el contador.
+(VACIO) 
 		@16384
 		D=A
 		@COORDENADA1
@@ -42,12 +42,12 @@
 		@BORRAR
 		0;JMP
 
-(LINEAS) // Indica las coordenadas desde donde se empiezan a pintar las lineas llevando el codigo a "cordpintar" y se reinicia el contador.
+(VERT) 
 		@16400 
 		D=A 
 		@COORDENADA2
 		M=D
-		@256 
+		@255 
 		D=A
 		@CUENTA2
 		M=D
@@ -67,7 +67,7 @@
 		@PINTAR
 		0;JMP
 
-(BLANCO) // Indica las coordenadas donde no se pintan las lineas y las manda a la orden de borrar.
+(HOR) 
 
 		@20480
 		D=A
@@ -91,10 +91,10 @@
 		@BORRAR
 		0;JMP
 
-(PINTAR) // Es un loop el cual hace que se pinte en las coordenadas indicadas y cuando termina hace un salto a "BLANCO".
+(PINTAR) 
 		@CUENTA2
 		D=M
-		@BLANCO
+		@HOR
 		D;JEQ
 
 		@COLOR
@@ -115,7 +115,7 @@
 		@PINTAR
 		0;JMP
 
-(BORRAR) // Es un loop el cual hace que se borre en las coordenadas indicadas y cuando termina hace un salto a "FINAL", que a su vez vuelve a iniciar el codigo.
+(BORRAR) 
 		@CUENTA1
 		D=M
 		@FINAL
@@ -139,6 +139,6 @@
 		@BORRAR
 		0;JMP
 
-(FINAL) // Da la orden de volver a "inicio" para poder usar nuevamente el programa.
+(FINAL) 
 		@INICIO
 		0;JMP
